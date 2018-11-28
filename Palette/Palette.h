211d3b7069097c5ef129a58415e57f0dad8d7c8c
Shawn Thye <shawn.thye@iproperty.com>
2018-11-29 03:09:30 +0800

    project init

---
+++ Palette/Palette.h
@@ -0,0 +1,19 @@
+//
+//  Palette.h
+//  Palette
+//
+//  Created by Shawn Thye on 29/11/2018.
+//  Copyright © 2018 Shawn Thye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Palette.
+FOUNDATION_EXPORT double PaletteVersionNumber;
+
+//! Project version string for Palette.
+FOUNDATION_EXPORT const unsigned char PaletteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Palette/PublicHeader.h>
+
+
